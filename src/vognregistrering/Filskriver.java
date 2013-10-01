5ccd24d63a4869f0dd31b1a14de7f064bf746d51
Andreas Mosti <andreas.mosti@gmail.com>
2013-10-01 19:22:29 +0200

    Init and basic classes and tests

---
+++ src/vognregistrering/Filskriver.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vognregistrering;
+
+/**
+ *
+ * @author andreasmosti
+ */
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class Filskriver {
+   private static File f; 
+   
+   public void setFilnavn(String filnavn){
+       f = new File(filnavn);
+   }
+  
+   public void skrivTilFil(ArrayList historikk) {
+      try{
+       FileOutputStream fos = new FileOutputStream(f);
+       ObjectOutputStream oos = new ObjectOutputStream(fos);
+       for(int i = 0; i < historikk.size(); i++) {
+           oos.writeObject(historikk.get(i));
+       }
+       fos.close();
+       oos.close();
+      }catch(IOException e){
+          System.out.println("Error while writing to file " + e.getMessage());
+            }
+        }
+        
+     }
